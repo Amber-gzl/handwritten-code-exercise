154337cd151a453bd5071dad9e1a8dc0e790b75e
hellolcb <1061748623@qq.com>
2020-07-31 09:53:05 +0800

    建库

---
+++ myBind.js
@@ -0,0 +1,9 @@
+// bind:把新的this绑定到某个函数func上，并返回func的一个拷贝
+// let boundFunc = func.bind(thisArg[, arg1[, arg2[, ...argN]]])
+// 1、apply、call与bind
+// 参考：https://blog.csdn.net/u010176097/article/details/80348447
+
+
+
+
+// 1、ES6 const
