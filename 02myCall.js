4c5a4969d5aa496c6407e6a92cd818d88aef414d
hellolcb <1061748623@qq.com>
2020-08-03 10:29:19 +0800

    call的实现

--- 02myCall.js
+++ 02myCall.js
@@ -21,12 +21,12 @@ console.log("typeof Symbol = ",typeof s);
 // instanceof运算符用于测试构造函数的prototype属性是否出现在对象的原型链中的任何位置。
 // 但是js是弱类型语言，字面量产出的原始数据类型无法使用instanceof判断。
 // 因此使用toString+call判断是最准确的
-console.log(Object.prototype.toString.call("zpp"));//[object String]
-console.log(Object.prototype.toString.call(4));//[object Number]
+console.log(Object.prototype.toString.call("gzl"));//[object String]
+console.log(Object.prototype.toString.call(9));//[object Number]
 console.log(Object.prototype.toString.call(true));//[object Boolean]
 console.log(Object.prototype.toString.call(undefined));//[object Undefined]
 console.log(Object.prototype.toString.call(null));//[object Null]
-console.log(Object.prototype.toString.call({name: "zpp"}));//[object Object]
+console.log(Object.prototype.toString.call({name: "gzl"}));//[object Object]
 console.log(Object.prototype.toString.call(function(){}));//[object Function]
 console.log(Object.prototype.toString.call([]));//[object Array]
 console.log(Object.prototype.toString.call(new Date));//[object Date]
@@ -34,7 +34,37 @@ console.log(Object.prototype.toString.call(/\d/));//[object RegExp]
 function Product(){};
 console.log(Object.prototype.toString.call(new Product));//[object Object]
 
-
-Function.prototype.myCall = function(){
-
+// call的实现
+// 在Function的原型链上添加myCall方法，使用...剩余运算符
+Function.prototype.myCall = function(context, ...args){
+    // 1、context为null/undefined时，将其指向windows
+    context = context || windows;
+    // 2、声明唯一变量名myCallFn，并为context添加myCallFn方法
+    const myCallFn = Symbol();
+    context[myCallFn] = this;
+    // context.myCallFn = this;
+    // 3、执行context上的函数myCallFn，使用...扩展运算符
+    const result = context[myCallFn](...args);
+    ///4、删除context上该方法
+    delete context[myCallFn];
+    ///5、返回执行结果
+    return result;
 }
+
+let obj1 = {
+    name: 'gzl',
+    school: 'sspku'
+}
+
+let obj2 = {
+    name: 'hy',
+    nickName: 'myBabyFish'
+}
+
+function whoAreU(){
+    console.log('arguments=', arguments);
+    console.log('this is ', this);
+}
+
+whoAreU.myCall(obj1,11,14);
+whoAreU.myCall(obj2,06,29);
